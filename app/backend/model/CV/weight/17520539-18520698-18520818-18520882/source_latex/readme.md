dc1e414d035d8b23473c755f8ea01b9171ee29b6
latruonghai@gmail.com <latruonghai@gmail.com>
2021-07-15 22:58:44 +0700

    new commit #2

--- app/backend/model/CV/weight/17520539-18520698-18520818-18520882/source_latex/readme.md
+++
@@ -1,4 +0,0 @@
-# Các ảnh trong pdf
-
-Đường dẫn:
-[Link ảnh](https://drive.google.com/drive/folders/17JpJip5znLkAjuAXPzWIBt8UuSTwvnB-?usp=sharing)
--- app/backend/model/CV/weight/17520539-18520698-18520818-18520882/sourcecode/CreateData.py
+++
@@ -1,7 +0,0 @@
-def createData(namefile='dijsktra_graph.pkl'):
-      with open(namefile, 'rb+') as f:
-    graph = pickle.load(f)
-  return graph
-
-if __name__ == "__main__":
-  graph_floyd, graph_dijsktra, graph_bellman =createData(namefile='floyd_path.pkl'), createData(), createData(namefile='bellman_graph.pkl')
